8a0dce28761d07d068eb7164c45122076cde2c00
Naman Tamrakar <namantam1@gmail.com>
2024-10-29 21:43:16 +0530

    Fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "639568795"
+    serviceWorkerVersion: "3963619840"
   }
 });
